407149f6340746c52159c82a55f33290da5aa531
priyanka <priyankagulbhile@gmail.com>
2023-01-19 18:47:44 +0530

    ArrayList more elements added

--- src/com/practice/ArrayListDemo.java
+++ src/com/practice/ArrayListDemo.java
@@ -3,11 +3,13 @@ package com.practice;
 import java.util.ArrayList;
 
 public class ArrayListDemo {
+	
 	public static void main(String[] args) {
 	ArrayList<Integer> al= new ArrayList<Integer>();
 	al.add(10);
 	al.add(20);
 	al.add(30);
+	al.add(50);
 	
 	for(int i: al) {
 		System.out.println(""+i);
